dec738681863ffc92bde7d5e239544d2e9fe67dd
Joshua Van Baalen <joshuavanbaalen@gmail.com>
2020-10-26 14:10:12 -0600

    Adding base springboot

---
+++ src/main/java/com/GroupA/Week4LabA/Week4LabAApplication.java
@@ -0,0 +1,13 @@
+package com.GroupA.Week4LabA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4LabAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4LabAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GroupA/Week4LabA/Week4LabAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GroupA.Week4LabA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4LabAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
