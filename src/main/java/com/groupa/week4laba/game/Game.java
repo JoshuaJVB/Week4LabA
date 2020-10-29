586dd0f5dc2801641b2426829c13609d99b5aea2
Andrew Strain <andrew.d.strain@outlook.com>
2020-10-29 08:58:51 -0400

    Create games

--- src/main/java/com/groupa/week4laba/game/Game.java
+++ src/main/java/com/groupa/week4laba/game/Game.java
@@ -1,2 +1,8 @@
-package com.groupa.week4laba.game;public interface Game {
+package com.groupa.week4laba.game;
+
+public interface Game {
+    void play();
+    Long getScore() throws UnsupportedOperationException;
+    Long[] getScores() throws UnsupportedOperationException;
+    int getLeaderboardLength();
 }
--- src/main/java/com/groupa/week4laba/game/Random.java
+++ src/main/java/com/groupa/week4laba/game/Random.java
@@ -1,2 +1,27 @@
-package com.groupa.week4laba.game;public class Random {
+package com.groupa.week4laba.game;
+
+public class Random implements Game {
+    private static final int LEADERBOARD_LENGTH = 5;
+
+    private Long score;
+
+    @Override
+    public void play() {
+        score = Math.round(Math.random() * Long.MAX_VALUE);
+    }
+
+    @Override
+    public Long getScore() throws UnsupportedOperationException {
+        return score;
+    }
+
+    @Override
+    public Long[] getScores() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getLeaderboardLength() {
+        return LEADERBOARD_LENGTH;
+    }
 }
