27c98899d2c0e5f9c072dfcb29a71581f5871ec6
Joshua Van Baalen <joshuavanbaalen@gmail.com>
2020-11-05 14:20:53 -0700

    created begining of selenium testing

--- src/test/java/com/groupa/week4laba/seleniumTests/JoshSelTest.java
+++ src/test/java/com/groupa/week4laba/seleniumTests/JoshSelTest.java
@@ -1,4 +1,57 @@
 package com.groupa.week4laba.seleniumTests;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JoshSelTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C://Users/tremu/selenium/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.navigate().to("http://localhost:8080");
+    }
+
+    @AfterEach
+    void teardown(){
+        driver.close();
+    }
+
+    @Test
+    void getTitle(){
+        assertEquals("Login", driver.getTitle());
+    }
+
+    @Test
+    void createUser(){
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body/div/form/input")).sendKeys("TestUser");
+        driver.findElement(By.xpath("/html/body/div/form/button")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        assertEquals("Pick a Game", driver.getTitle());
+    }
+
+    @Test
+    void getToSnakeEyes () throws InterruptedException {
+        driver.findElement(By.xpath("/html/body/div/form/input")).sendKeys("TestUser");
+        driver.findElement(By.xpath("/html/body/div/form/button")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/form/span[1]/button")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+    }
 }
